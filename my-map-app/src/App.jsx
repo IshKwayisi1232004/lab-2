18cebd03b8fb330a29301c158a65fc5bbebd8d59
Ishmael Kwayisi <ishmael.kwayisi@snhu.edu>
2025-10-09 16:21:11 -0400

    Maerker is somewhat working now

--- my-map-app/src/App.jsx
+++ my-map-app/src/App.jsx
@@ -3,11 +3,12 @@ import { useEffect } from 'react'
 //import viteLogo from '/vite.svg'
 import './App.css'
 import 'leaflet/dist/leaflet.css'
-import L, { popup } from 'leaflet'
+import L from 'leaflet'
+import kirboImg from './assets/Kirbo.png'
 
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+// import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+// import markerIcon from 'leaflet/dist/images/marker-icon.png';
+// import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 
 
@@ -27,36 +28,51 @@ function App() {
       const mapContainer = document.getElementById("map");
       if (!mapContainer) return; // prevent Leaflet from running too early
 
-      L.Icon.Default.mergeOptions({
-         iconRetinaUrl: markerIcon2x,
-         iconUrl: markerIcon,
-         shadowUrl: markerShadow,
-     });
+    //   L.Icon.Default.mergeOptions({
+    //      iconRetinaUrl: markerIcon2x,
+    //      iconUrl: markerIcon,
+    //      shadowUrl: markerShadow,
+    //  });
 
 
       {/* Initialize the map and set its view */}
       var map = L.map('map').setView([51.505, -0.09], 13);
 
       // Initalize the marker
-      const marker = L.marker([51.5, -0.09]).addTo(map);
+      // const marker = L.marker([51.5, -0.09]).addTo(map);
 
-      var circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-      }).addTo(map);
+      // var circle = L.circle([51.508, -0.11], {
+      //   color: 'red',
+      //   fillColor: '#f03',
+      //   fillOpacity: 0.5,
+      //   radius: 500
+      // }).addTo(map);
 
     //map.on('click', onMapClick);
 
+     const kirboIcon = L.icon({
+        iconUrl: kirboImg, // place in /public or import from assets
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+    });
+
+
     // Map click popup
-    const popup = L.popup();
+    //const popup = L.popup();
     map.on('click', (e) => {
-      alert("You clicked the map at " + e.latlng);
-      popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+      // alert("You clicked the map at " + e.latlng);
+      // popup
+      //   .setLatLng(e.latlng)
+      //   .setContent("You clicked the map at " + e.latlng.toString())
+      //   .openOn(map);
+
+      L.marker(e.latlng, { icon: kirboIcon }).addTo(map);
+
+      const {lat, lng} = e.latlng;
+
+      const marker = L.marker([lat, lng]).addTo(map);
+
+      marker.bindPopup(`Marker added at<br>${lat.toFixed(4)}, ${lng.toFixed(4)}`).openPopup();
     });
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -64,10 +80,10 @@ function App() {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    //Marker
-    marker.bindPopup("Hello from London!").openPopup();
+    // //Marker
+    // marker.bindPopup("Hello from London!").openPopup();
 
-    circle.bindPopup("I am a circle.");
+    // circle.bindPopup("I am a circle.");
 
     // Cleanup when component unmounts (important for hot reloads)
     return () => {
@@ -84,4 +100,5 @@ function App() {
   );
 }
 
-export default App
+
+export default App;
